3cfe70902206a5505f38f347a30c72e4767af7e5
Yajushreshtha <97739937+Soyvor@users.noreply.github.com>
2023-10-06 13:19:02 +0530

    Create name.java

---
+++ name.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class name
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter your name");
+      String name=sc.nextLine();
+      
+  }
